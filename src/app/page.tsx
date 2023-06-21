606b82daf79715e5892df0d176f0bdff518a7397
Elogin <86604232+elogin91@users.noreply.github.com>
2023-06-21 15:09:39 +0200

    add frontmatter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,53 @@
-import Image from 'next/image'
+import fs from "fs";
+import { join } from 'path'
+import { serialize } from "next-mdx-remote/serialize"
+import { MDXRemote } from "next-mdx-remote";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to Cristina Dev portfolio.</h1>
-    </main>
+const projectsDirectory = join(process.cwd(), '/projects')
+
+function getProjectSlugs() {
+  return fs.readdirSync(projectsDirectory)
+}
+
+async function getFirstProjectsInfo(){
+  const directoryContent = fs.readdirSync(projectsDirectory)
+
+  return Promise.all(directoryContent.map( p => getProjectFronmatter(p)))
+}
+
+async function getProjectFronmatter(projectPath: string) {
+  const source = fs.readFileSync(
+    join(projectsDirectory, projectPath),
+    'utf8'
+  );
+
+  return serialize<Record<string, unknown>, Record<string, "name" | "technologies" | "date">>(source, { parseFrontmatter: true });
+}
+
+export default async function Home() {
+  const projects = await getFirstProjectsInfo();
+
+
+  return (<>
+  <h1>Welcome from Cristina Dev</h1>
+  <h2>My portfolio</h2>
+
+
+    <h3>Develop Projects</h3>
+
+    <ul>
+      {projects.map((p) => {
+        return (
+          <li key={p.frontmatter.name}>
+            Nombre: {p.frontmatter.name} <br />
+            Tecnologías: {p.frontmatter.technologies} <br />
+            Fecha: {p.frontmatter.date} <br />
+          </li>
+
+        );
+      })}
+    </ul>
+
+  </>
   )
 }
---
+++ src/app/projects.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+const BlogPage = ({  }) => {
+    return (
+        <section>
+            {
+            }
+        </section>
+    )
+}
+
+import HelloWorld from '../../projects/cronus13/hellow.mdx'
+ 
+export default function projects() {
+  return <HelloWorld />
+}
