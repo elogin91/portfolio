3ab8a5d8f56a22edff726d306ce6f3257dd341f0
Elogin <86604232+elogin91@users.noreply.github.com>
2023-08-16 12:57:51 +0200

    creating form items

--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -2,13 +2,34 @@ import React from "react";
 
 export const Form = () => {
     return(
-        <form>
+        <form className="bg-blue p-10">
             <div className="mb-4 ">
-                <label className="font-bold mb-2 text-night" htmlFor='name'>
+                <label className="label-form" htmlFor='name'>
                     Name 
                 </label>
-                <input id='name' name='name' type="text" className="border border-puce p-2 w-full"/>
+                <input id='name' name='name' type="text" className="input-form"/>
             </div>
+
+            <div className="mb-4 ">
+                <label className="label-form" htmlFor='company'>
+                    Company 
+                </label>
+                <input id='company' name='company' type="text" className="input-form"/>
+            </div>
+
+            <div className="mb-4 ">
+                <label className="label-form" htmlFor='email'>
+                    Email
+                </label>
+                <input id='email' name='email' type="email" className="input-form"/>
+            </div>
+            <div className="mb-4 ">
+                <label className="label-form" htmlFor='message'>
+                    Message
+                </label>
+                <textarea id='message' name='mesagge' className="input-form"/>
+            </div>
+            <button type="submit"className="button-form">Send Message</button>
         </form>
     )
 }
