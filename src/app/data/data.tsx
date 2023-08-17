d3f7bad996d4143652abe20506594b6fd579ecaf
Elogin <86604232+elogin91@users.noreply.github.com>
2023-08-17 17:07:53 +0200

    updating data

--- src/app/data/data.tsx
+++ src/app/data/data.tsx
@@ -19,117 +19,50 @@ export const pages = [
 export const aboutMeData = {
 	name: "Cristina Front End Developer",
 	title: "Welcome! My name is Cristina Álvarez",
-	body: "TODO A software engineer with a passion for solving problems with code. I started my journey into the world of software development a few years ago, and since then I've been constantly learning and improving my skills. My background is in computer science, but I'm always looking for opportunities to branch out and explore new technologies.",
+	body: "A software developer with a passion for creatting new world with code. I started my journey into the world of software development a few years ago, and since then I've been constantly learning and improving my skills. My background is in administrative magement, but I'm always looking for opportunities to branch out and explore new technologies.",
 	highlightedBody:
-		"TODO As a beginner, I'm constantly seeking new challenges and projects to work on. I'm particularly interested in web development, machine learning, and game development, and I've completed several personal projects in these areas. I'm always eager to take on new challenges and collaborate with others, and I believe that working on projects is the best way to learn and grow as a software engineer.",
+		"I'm looking for my first work oportunity in the develop web. I'm constantly seeking new challenges and projects to work on. I'm particularly interested in web development, and I've completed several personal projects in these areas. I'm always eager to take on new challenges and collaborate with others, and I believe that working on projects is the best way to learn and grow as a software developer.. If you're interested in learning more about my skills and experience, feel free to contact with me. I'd love to hear from you and discuss potential opportunities to collaborate on projects or learn from each other",
 	body2:
-		"TODO When I'm not coding, I enjoy spending time with my family and friends, watching movies, and playing video games. I'm a lifelong learner, and I'm always looking for new books, tutorials, and courses to help me improve my skills and knowledge. If you're interested in learning more about my skills and experience, feel free to take a look at my CV. I'd love to hear from you and discuss potential opportunities to collaborate on projects or learn from each other.",
+		"In my free time I like to travel, read books... my favorite activity is to throw myself on the sofa to watch series and movies with my dogs and family.",
 };
 
 export const contactMeText = {
-	title: "Hello there! ",
-	body: "I am a data scientist and machine learning engineer with a passion for solving complex problems with data.  I bring a unique blend of technical and interpersonal skills to the table. I'm always eager to take on new challenges and collaborate with like-minded individuals. If you'd like to get in touch, simply fill out the form below and I'll get back to you as soon as possible. Looking forward to hearing from you!",
+	title: "Hi! What's up? ",
+	body: "I am a web developer and law graduate with a passion for creat new pretty, secures, and cuality applications.I bring a new energy and unique blend of technical and autolearning. I'm always eager to take on new challenges and collaborate with others technicals. If you'd like to get in touch, simply fill out the form below and I'll get back to you as soon as possible. Im waitting to know you and learn and work more about your project!",
 };
 
 export const projects = [
 	{
-		title: "Personal Finance Tracker",
+		title: "",
 		body1:
-			"A web application that allows users to track their income and expenses, categorize transactions, and view reports and charts to gain insights into their spending habits.",
-		body2: "Technologies used: HTML, CSS, JavaScript, and Flask.",
-		image: "luke-chesser-JKUTrJ4vK00-unsplash.jpg",
+			"A web application ...",
+		body2: "Technologies used: HTML, CSS, JavaScript.",
+		image: "prueba.jpg",
 		links: [
 			{
 				text: "Source Code",
-				url: "https://github.com/richardbrown/personal-finance-tracker",
+				url: "https://github.com/sdasdasdasdaadsads",
 			},
 			{
 				text: "Live Site",
-				url: "https://personal-finance-tracker.richardbrown.net",
+				url: "https://asdasdadasd.es",
 			},
 		],
 	},
+
 	{
-		title: "Movie Recommendation System",
-		body1:
-			"A machine learning model that recommends movies to users based on their viewing history and preferences.",
-		body2: "Technologies used: Python, Pandas, NumPy, and scikit-learn",
-		image: "charlesdeluvio-jtmwD4i4v1U-unsplash.png",
+		title: "",
+		body1: " ",
+		body2: "Technologies used:",
+		image: ".png",
 		links: [
 			{
 				text: "Source Code",
-				url: "https://github.com/richardbrown/weather-app",
+				url: "https://github.com",
 			},
 			{
 				text: "Live Site",
-				url: "https://weather-app.richardbrown.net",
-			},
-		],
-	},
-	{
-		title: "Tic-Tac-Toe Game",
-		body1: "A simple game of Tic-Tac-Toe built with Python and Pygame. ",
-		body2: "Technologies used: Python and Pygame.",
-		image: "tic_tac_toe.png",
-		links: [
-			{
-				text: "Source Code",
-				url: "https://github.com/richardbrown/tic-tac-toe-app",
-			},
-			{
-				text: "Live Site",
-				url: "https://todo-list-app.richardbrown.net",
-			},
-		],
-	},
-	{
-		title: "Weather App",
-		body1:
-			"A web application that allows users to search for the current weather conditions in any city in the world.",
-		body2: "Technologies used: HTML, CSS, JavaScript, and OpenWeather API.",
-		image: "luke-chesser-JKUTrJ4vK00-unsplash.jpg",
-		links: [
-			{
-				text: "Source Code",
-				url: "https://github.com/richardbrown/weather-app",
-			},
-			{
-				text: "Live Site",
-				url: "https://weather-app.richardbrown.net",
-			},
-		],
-	},
-	{
-		title: "Todo List App",
-		body1:
-			"A simple todo list application that allows users to add, edit, and delete tasks, and mark them as complete.",
-		body2: "Technologies used: HTML, CSS, JavaScript, and local storage.",
-		image: "luke-chesser-JKUTrJ4vK00-unsplash.jpg",
-		links: [
-			{
-				text: "Source Code",
-				url: "https://github.com/richardbrown/todo-list-app",
-			},
-			{
-				text: "Live Site",
-				url: "https://todo-list-app.richardbrown.net",
-			},
-		],
-	},
-	{
-		title: "Recipe Book",
-		body1:
-			"A web application that allows users to search for and save their favorite recipes from around the world.",
-		body2: "Technologies used: HTML, CSS, JavaScript, and Edamam API.",
-		image: "luke-chesser-JKUTrJ4vK00-unsplash.jpg",
-		links: [
-			{
-				text: "Source Code",
-				url: "https://github.com/richardbrown/recipe-book",
-			},
-			{
-				text: "Live Site",
-				url: "https://recipe-book.richardbrown.net",
+				url: "https://.es",
 			},
 		],
 	},
