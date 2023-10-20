d769dc8e8b75344b497c8bcb1d5cf24bf38c6882
Cristina Álvarez López <elo.programacion@gmail.com>
2023-10-20 10:46:59 +0200

    resolving conflict

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -57,12 +57,7 @@ export const Header = ({ pages, name }: HeaderProps) => {
               leaveFrom="transform opacity-100 scale-100"
               leaveTo="transform opacity-0 scale-95"
             >
-
-<<<<<<< HEAD
-              <Menu.Items className="items-center absolute right-5 z-10 mt-24 w-88 origin-top-right rounded-md bg-puce shadow-lg sm:hidden">
-=======
-              <Menu.Items className="absolute right-2 z-10 mt-24 w-100 origin-top-right rounded-md bg-puce shadow-lg ">
->>>>>>> 86a48dafc20b13b8da66af095ba501030c78fcef
+              <Menu.Items className="items-center absolute right-2 z-10 mt-24 w-96 origin-top-right rounded-md bg-puce shadow-lg sm:hidden">
                 <div
                   id="navbar-default"
                   className='py-1' >
